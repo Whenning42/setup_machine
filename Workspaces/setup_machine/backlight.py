66e00a477f636864da05d152cef4dedb33e93f6a
William Henning <wchenningwchenning@gmail.com>
2023-10-31 20:55:57 -0400

    Add screen brightness script.

---
+++ Workspaces/setup_machine/backlight.py
@@ -0,0 +1,24 @@
+import argparse
+import subprocess
+
+
+def run(cmd):
+    return subprocess.check_output(cmd, shell=True).decode("utf-8").strip()
+
+
+# Add argument for inc and dec mode
+parser = argparse.ArgumentParser()
+parser.add_argument("--inc", action="store_true", help="increase the brightness")
+parser.add_argument("--dec", action="store_true", help="decrease the brightness")
+args = parser.parse_args()
+
+brightness = run("xbacklight")
+brightness = round(float(brightness))
+delta = 10
+if brightness == 0 and args.inc:
+    delta = 1
+
+if args.inc:
+    run("xbacklight -inc {}".format(delta))
+elif args.dec:
+    run("xbacklight -dec {}".format(delta))
